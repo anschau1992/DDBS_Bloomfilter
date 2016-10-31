3a4ac402d3dfbbb62726a4621e06ab78d28aeeb9
Andreas Schaufelbühl <andreas.schaufelbuehl@uzh.ch>
2016-10-31 18:19:34 +0100

    test

---
+++ local/main/java/BloomFilter.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Andy on 31.10.16.
+ */
+
+import java.util.BitSet;
+import java.util.Random;
+
+public class BloomFilter {
+    private BitSet bitSet;
+
+    final int [] hashSeeds;
+
+    public 
+
+}
